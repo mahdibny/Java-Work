09bcc5507fa11644df2429cbcb4bd0f74f3cf5c0
mahdibny <mahdibny@gmail.com>
2016-01-05 03:30:22 +0000

    finishing touch hw2

--- Eric/Hw2/Account.java
+++ Eric/Hw2/Account.java
@@ -9,38 +9,9 @@ public class Account{
     private double balance;
     
     public Account(int accountID, int pin, double balance){
-        
         setAccountID(accountID);
         setBalance(balance);
         setPin(pin);
-        /*
-        if (accountID>=1000 & accountID<=9999){
-            this.accountID=accountID;
-        }
-        else {
-            System.out.println("There was an error in creating your ID.\nInputs for ID must be from 1000 to 9999." 
-            + " Your accountID has been defaulted to 9999.");
-            this.accountID=9999;
-        }
-        
-         if (pin>=1000 & pin<=9999){
-            this.pin=pin;
-        }
-        else {
-            System.out.println("There was an error in creating your pin.\nInputs for pin must be from 1000 to 9999." 
-            + " Your accountID has been defaulted to 9999.");
-            this.pin=0;
-        }
-        
-        if (balance>0){
-            this.balance=balance;
-        }
-        else{
-            System.out.println("There was an error in creating your balnce.\nInputs for balance must be greater than 0." 
-            + "Your balance has been defaulted to 0.");
-            this.balance=0;
-        }
-        */
     }
     
     // Set Methods
@@ -99,15 +70,9 @@ public class Account{
         // returns the value of the ID and the balance of the specific object 
     }
     
-    
     // comparison between objects
     public boolean equals(Account obj){
-        if (obj instanceof Account){
             return (pin==((Account)obj).getPin() & accountID==((Account)obj).getAccountID());
             // checks to see if the account pin and ID are the same for two objects
-        }
-        else{
-            return false;
-        }
     }
 }
